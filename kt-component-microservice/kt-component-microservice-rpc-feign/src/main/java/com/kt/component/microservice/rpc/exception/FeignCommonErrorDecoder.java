cc48b5b49c9300e3e75c74481ea08a5055e1547c
Javis <javischen9548@foxmail.com>
2022-02-21 16:43:44 +0800

    u

--- kt-component-microservice/kt-component-microservice-rpc-feign/src/main/java/com/kt/component/microservice/rpc/exception/FeignCommonErrorDecoder.java
+++ kt-component-microservice/kt-component-microservice-rpc-feign/src/main/java/com/kt/component/microservice/rpc/exception/FeignCommonErrorDecoder.java
@@ -1,13 +1,50 @@
 package com.kt.component.microservice.rpc.exception;
 
+import cn.hutool.core.io.IoUtil;
+import com.alibaba.fastjson.JSONObject;
 import feign.Response;
 import feign.codec.ErrorDecoder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@Slf4j
 public class FeignCommonErrorDecoder implements ErrorDecoder {
 
     @Override
-    public Exception decode(String s, Response response) {
+    public Exception decode(String methodKey, Response response) {
+        int status = response.status();
         Response.Body body = response.body();
-        return new RpcException(null, body.toString());
+        String bodyString = readFromBody(body);
+        if (status == HttpStatus.UNAUTHORIZED.value()) {
+            String msg = getMsgFromBody(bodyString);
+            String service = getServiceFromBody(bodyString);
+            return new RpcException(service, response, msg);
+        } else if (status == HttpStatus.FORBIDDEN.value()) {
+            String msg = getMsgFromBody(bodyString);
+            String service = getServiceFromBody(bodyString);
+            return new RpcException(service, response, msg);
+        }
+        return new RpcException(null, response, bodyString);
+    }
+
+    private String getMsgFromBody(String body) {
+        return JSONObject.parseObject(body).getString("msg");
+    }
+
+    private String getServiceFromBody(String body) {
+        return JSONObject.parseObject(body).getString("service");
+    }
+
+    private String readFromBody(Response.Body body) {
+        String read = "";
+        try {
+            read = IoUtil.read(body.asInputStream(), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            log.error("读取输入流异常", e);
+        }
+        return read;
     }
 }
