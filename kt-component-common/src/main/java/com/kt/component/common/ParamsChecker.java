bdb9bb06723966636b49766047ced44c986d7b1b
Javis <javischen9548@foxmail.com>
2022-03-05 15:16:44 +0800

    u

--- kt-component-common/src/main/java/com/kt/component/common/ParamsChecker.java
+++ kt-component-common/src/main/java/com/kt/component/common/ParamsChecker.java
@@ -1,13 +1,19 @@
 package com.kt.component.common;
 
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.Collection;
 import java.util.Objects;
 
 public class ParamsChecker {
 
     public static void throwIfIsEmpty(String str, RuntimeException exception) {
         throwIfIsTrue(StringUtils.isEmpty(str), exception);
+    }
+
+    public static void throwIfIsEmpty(Collection<?> collection, RuntimeException exception) {
+        throwIfIsTrue(CollectionUtils.isEmpty(collection), exception);
     }
 
     public static void throwIfIsBlank(String str, RuntimeException exception) {
