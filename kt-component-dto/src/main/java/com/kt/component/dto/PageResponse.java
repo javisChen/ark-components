59fa136d0cfe589a05eacd4e13b5df7f595c4a20
Javis <javischen9548@foxmail.com>
2022-03-08 13:47:51 +0800

    u

--- kt-component-dto/src/main/java/com/kt/component/dto/PageResponse.java
+++ kt-component-dto/src/main/java/com/kt/component/dto/PageResponse.java
@@ -13,15 +13,15 @@ import java.util.Collection;
 public class PageResponse<T> {
 
     @ApiModelProperty(value = "总页数", required = false, notes = "")
-    private Long total = 0L;
+    private Integer total = 0;
     @ApiModelProperty(value = "每页显示数量", required = false, notes = "")
-    private Long size = 1L;
+    private Integer size = 1;
     @ApiModelProperty(value = "每页显示数量", required = false, notes = "")
-    private Long current = 1L;
+    private Integer current = 1;
     @ApiModelProperty(value = "数据", required = false, notes = "")
     private Collection<T> records;
 
-    public PageResponse(long current, long size,long total, Collection<T> records) {
+    public PageResponse(int current, int size, int total, Collection<T> records) {
         this.total = total;
         this.size = size;
         this.current = current;
@@ -32,11 +32,11 @@ public class PageResponse<T> {
     }
 
     public static <T> PageResponse<T> build(long current, long size, long total, Collection<T> records) {
-        return new PageResponse<>(current, size, total, records);
+        return new PageResponse<>((int)current, (int)size, (int)total, records);
     }
 
     public static <T> PageResponse<T> build(IPage<T> page) {
-        return new PageResponse<>(page.getCurrent(), page.getSize(), page.getTotal(), page.getRecords());
+        return new PageResponse<>((int)page.getCurrent(), (int)page.getSize(), (int)page.getTotal(), page.getRecords());
     }
 
 }
