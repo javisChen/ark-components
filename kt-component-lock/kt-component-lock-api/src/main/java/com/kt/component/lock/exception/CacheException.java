20c177fad9b3c01b4328031101ccb04d23202a81
Javis <javischen9548@foxmail.com>
2022-03-25 15:42:04 +0800

    u

---
+++ kt-component-lock/kt-component-lock-api/src/main/java/com/kt/component/lock/exception/CacheException.java
@@ -0,0 +1,12 @@
+package com.kt.component.cache.exception;
+
+/**
+ * Cache操作异常类
+ * @author victor
+ */
+public class CacheException extends RuntimeException {
+
+    public CacheException(Throwable cause) {
+        super(cause);
+    }
+}
