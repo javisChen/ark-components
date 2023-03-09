52ebd8766263cc4b1e8b3a47310c0d3ebc3ad9e1
Javis <javischen9548@foxmail.com>
2023-03-09 18:39:53 +0800

    u

--- ark-component-mq/ark-component-mq-integration-spring-boot-starter/src/test/java/com/ark/component/mq/integration/rocket/RocketMQProducerTest.java
+++ ark-component-mq/ark-component-mq-integration-spring-boot-starter/src/test/java/com/ark/component/mq/integration/rocket/RocketMQProducerTest.java
@@ -12,18 +12,18 @@ import org.apache.rocketmq.remoting.exception.RemotingException;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 
-public class RocketMQProducerTest extends ApplicationTests {
+public class RocketMQProducerTest {
 
     public static void main(String[] args) throws MQBrokerException, RemotingException, InterruptedException, MQClientException {
         DefaultMQProducer defaultMQProducer = new DefaultMQProducer();
         defaultMQProducer.setProducerGroup("pg_edfault");
-        defaultMQProducer.setNamesrvAddr("localhost:9876");
+        defaultMQProducer.setNamesrvAddr("rocket.cloud.org:9876");
         defaultMQProducer.start();
 
-        for (int i = 0; i < 1; i++) {
-            Message message = new Message(MQTestConst.TOPIC,
-                    MQTestConst.TAG_BROAD_CASTING,
-                    ("TEST MSG：" + i).getBytes(StandardCharsets.UTF_8));
+        for (int i = 0; i < 5; i++) {
+            Message message = new Message(MQTestConst.TOPIC2,
+                    MQTestConst.TAG_CLUSTER,
+                    ("HELLO TEST MSG：" + i).getBytes(StandardCharsets.UTF_8));
             message.setDelayTimeLevel(DelayLevel._10S.getValue());
             SendResult send = defaultMQProducer.send(message);
             System.out.println("发送成功：" + LocalDateTime.now());
