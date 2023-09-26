8a21e1fa779b53c2b00f34c255935c602e6189c1
Javis <javischen9548@foxmail.com>
2023-09-26 18:23:16 +0800

    u

--- ark-component-common/ark-component-common-core/src/main/java/com/ark/component/common/util/assemble/FieldsAssembler.java
+++ ark-component-common/ark-component-common-core/src/main/java/com/ark/component/common/util/assemble/FieldsAssembler.java
@@ -6,6 +6,7 @@ import org.apache.commons.collections4.MapUtils;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.function.BiConsumer;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -64,5 +65,62 @@ public class FieldsAssembler {
         }
         records.forEach(record -> field.accept(record, map.get(recordId.apply(record))));
     }
+
+    /**
+     * 装配字段
+     *
+     * @param condition  判断是否需要进行装配
+     * @param record     需要装配的数据
+     * @param recordId   数据行id
+     * @param field      需要装配的字段
+     * @param datasource 数据源
+     * @param <RECORD>   需要被装配的数据
+     * @param <SOURCE>   待装配的数据源
+     */
+//    public static <RECORD, SOURCE> void execute(boolean condition,
+//                                                RECORD record,
+//                                                Function<? super RECORD, Long> recordId,
+//                                                BiConsumer<RECORD, List<SOURCE>> field,
+//                                                Function<Long, List<SOURCE>> datasource) {
+//        if (!condition) {
+//            return;
+//        }
+//        Long id = recordId.apply(record);
+//
+//        List<SOURCE> targets = datasource.apply(id);
+//        if (CollectionUtils.isEmpty(targets)) {
+//            return;
+//        }
+//
+//        field.accept(record, targets);
+//    }
+
+    /**
+     * 装配字段
+     *
+     * @param condition  判断是否需要进行装配
+     * @param record     需要装配的数据
+     * @param recordId   数据行id
+     * @param field      需要装配的字段
+     * @param datasource 数据源
+     * @param <RECORD>   需要被装配的数据
+     * @param <SOURCE>   待装配的数据源
+     */
+    public static <RECORD, SOURCE> void execute(RECORD record,
+                                                Function<? super RECORD, Long> recordId,
+                                                BiConsumer<RECORD, SOURCE> field,
+                                                Function<Long, SOURCE> datasource) {
+        if (record == null) {
+            return;
+        }
+        Long id = recordId.apply(record);
+
+        SOURCE target = datasource.apply(id);
+        if (Objects.isNull(target)) {
+            return;
+        }
+        field.accept(record, target);
+    }
+
 }
 
