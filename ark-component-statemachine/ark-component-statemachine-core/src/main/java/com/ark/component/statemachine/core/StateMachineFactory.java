3e5c510e6f78f4db26d91535231cc9d992e89a79
Javis <javischen9548@foxmail.com>
2023-11-27 14:21:24 +0800

    u

--- ark-component-statemachine/ark-component-statemachine-core/src/main/java/com/ark/component/statemachine/core/StateMachineFactory.java
+++ ark-component-statemachine/ark-component-statemachine-core/src/main/java/com/ark/component/statemachine/core/StateMachineFactory.java
@@ -15,23 +15,23 @@ public class StateMachineFactory implements InitializingBean, ApplicationContext
     private ApplicationContext applicationContext;
 
     @SuppressWarnings("rawtypes")
-    private static final Map<String, SimpleStateMachine> machines = new HashMap<>(16);
+    private static final Map<String, StateMachine> machines = new HashMap<>(16);
 
     @SuppressWarnings("unchecked")
-    public <S, E> SimpleStateMachine<S, E> get(String machineId) {
+    public <S, E> StateMachine<S, E> get(String machineId) {
         return machines.get(machineId);
     }
 
-    public <S, E> void register(String machineId, SimpleStateMachine<S, E> simpleStateMachine) {
+    public <S, E> void register(String machineId, StateMachine<S, E> StateMachine) {
         if (machines.containsKey(machineId)) {
             throw new StateMachineException("{} machineId has been registered");
         }
-        machines.put(machineId, simpleStateMachine);
+        machines.put(machineId, StateMachine);
     }
 
     @Override
     public void afterPropertiesSet() {
-        Map<String, SimpleStateMachine> beans = applicationContext.getBeansOfType(SimpleStateMachine.class);
+        Map<String, StateMachine> beans = applicationContext.getBeansOfType(StateMachine.class);
         beans.forEach((beanName, machine) -> this.register(machine.getMachineId(), machine));
     }
 
