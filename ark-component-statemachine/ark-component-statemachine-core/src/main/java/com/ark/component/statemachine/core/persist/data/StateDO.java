6cfb1d79f51898f5c49eb8599964f0a9dccebea2
javisChen <javischen9548@foxmail.com>
2024-01-04 21:22:35 +0800

    u

--- ark-component-statemachine/ark-component-statemachine-core/src/main/java/com/ark/component/statemachine/core/persist/data/StateDO.java
+++ ark-component-statemachine/ark-component-statemachine-core/src/main/java/com/ark/component/statemachine/core/persist/data/StateDO.java
@@ -1,72 +1,72 @@
-package com.ark.component.statemachine.core.persist.data;
-
-import java.time.LocalDateTime;
-
-public class StateDO {
-
-    private Long id;
-
-    private String machineId;
-    private String bizId;
-
-    private String state;
-    private Integer ended;
-    private LocalDateTime gmtCreate;
-
-    private Long creator;
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getMachineId() {
-        return machineId;
-    }
-
-    public void setMachineId(String machineId) {
-        this.machineId = machineId;
-    }
-
-    public String getBizId() {
-        return bizId;
-    }
-
-    public void setBizId(String bizId) {
-        this.bizId = bizId;
-    }
-
-    public Integer getEnded() {
-        return ended;
-    }
-
-    public void setEnded(Integer ended) {
-        this.ended = ended;
-    }
-
-    public LocalDateTime getGmtCreate() {
-        return gmtCreate;
-    }
-
-    public void setGmtCreate(LocalDateTime gmtCreate) {
-        this.gmtCreate = gmtCreate;
-    }
-
-    public Long getCreator() {
-        return creator;
-    }
-
-    public void setCreator(Long creator) {
-        this.creator = creator;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-}
+//package com.ark.component.statemachine.core.persist.data;
+//
+//import java.time.LocalDateTime;
+//
+//public class StateDO {
+//
+//    private Long id;
+//
+//    private String machineId;
+//    private String bizId;
+//
+//    private String state;
+//    private Integer ended;
+//    private LocalDateTime createTime;
+//
+//    private Long creator;
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getMachineId() {
+//        return machineId;
+//    }
+//
+//    public void setMachineId(String machineId) {
+//        this.machineId = machineId;
+//    }
+//
+//    public String getBizId() {
+//        return bizId;
+//    }
+//
+//    public void setBizId(String bizId) {
+//        this.bizId = bizId;
+//    }
+//
+//    public Integer getEnded() {
+//        return ended;
+//    }
+//
+//    public void setEnded(Integer ended) {
+//        this.ended = ended;
+//    }
+//
+//    public LocalDateTime getcreateTime() {
+//        return createTime;
+//    }
+//
+//    public void setcreateTime(LocalDateTime createTime) {
+//        this.createTime = createTime;
+//    }
+//
+//    public Long getCreator() {
+//        return creator;
+//    }
+//
+//    public void setCreator(Long creator) {
+//        this.creator = creator;
+//    }
+//
+//    public String getState() {
+//        return state;
+//    }
+//
+//    public void setState(String state) {
+//        this.state = state;
+//    }
+//}
--- ark-component-statemachine/ark-component-statemachine-core/src/main/java/com/ark/component/statemachine/core/persist/data/StateHistoryDO.java
+++ ark-component-statemachine/ark-component-statemachine-core/src/main/java/com/ark/component/statemachine/core/persist/data/StateHistoryDO.java
@@ -1,90 +1,90 @@
-package com.ark.component.statemachine.core.persist.data;
-
-import java.time.LocalDateTime;
-
-public class StateHistoryDO {
-    private Long id;
-    private String machineId;
-    private String bizId;
-    private String currentState;
-
-    private String preState;
-
-    private String extras;
-
-    private String remark;
-    private LocalDateTime gmtCreate;
-    private Long creator;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getMachineId() {
-        return machineId;
-    }
-
-    public void setMachineId(String machineId) {
-        this.machineId = machineId;
-    }
-
-    public String getBizId() {
-        return bizId;
-    }
-
-    public void setBizId(String bizId) {
-        this.bizId = bizId;
-    }
-
-    public String getCurrentState() {
-        return currentState;
-    }
-
-    public void setCurrentState(String currentState) {
-        this.currentState = currentState;
-    }
-
-    public String getPreState() {
-        return preState;
-    }
-
-    public void setPreState(String preState) {
-        this.preState = preState;
-    }
-
-    public String getExtras() {
-        return extras;
-    }
-
-    public void setExtras(String extras) {
-        this.extras = extras;
-    }
-
-    public String getRemark() {
-        return remark;
-    }
-
-    public void setRemark(String remark) {
-        this.remark = remark;
-    }
-
-    public LocalDateTime getGmtCreate() {
-        return gmtCreate;
-    }
-
-    public void setGmtCreate(LocalDateTime gmtCreate) {
-        this.gmtCreate = gmtCreate;
-    }
-
-    public Long getCreator() {
-        return creator;
-    }
-
-    public void setCreator(Long creator) {
-        this.creator = creator;
-    }
-}
+//package com.ark.component.statemachine.core.persist.data;
+//
+//import java.time.LocalDateTime;
+//
+//public class StateHistoryDO {
+//    private Long id;
+//    private String machineId;
+//    private String bizId;
+//    private String currentState;
+//
+//    private String preState;
+//
+//    private String extras;
+//
+//    private String remark;
+//    private LocalDateTime createTime;
+//    private Long creator;
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getMachineId() {
+//        return machineId;
+//    }
+//
+//    public void setMachineId(String machineId) {
+//        this.machineId = machineId;
+//    }
+//
+//    public String getBizId() {
+//        return bizId;
+//    }
+//
+//    public void setBizId(String bizId) {
+//        this.bizId = bizId;
+//    }
+//
+//    public String getCurrentState() {
+//        return currentState;
+//    }
+//
+//    public void setCurrentState(String currentState) {
+//        this.currentState = currentState;
+//    }
+//
+//    public String getPreState() {
+//        return preState;
+//    }
+//
+//    public void setPreState(String preState) {
+//        this.preState = preState;
+//    }
+//
+//    public String getExtras() {
+//        return extras;
+//    }
+//
+//    public void setExtras(String extras) {
+//        this.extras = extras;
+//    }
+//
+//    public String getRemark() {
+//        return remark;
+//    }
+//
+//    public void setRemark(String remark) {
+//        this.remark = remark;
+//    }
+//
+//    public LocalDateTime getcreateTime() {
+//        return createTime;
+//    }
+//
+//    public void setcreateTime(LocalDateTime createTime) {
+//        this.createTime = createTime;
+//    }
+//
+//    public Long getCreator() {
+//        return creator;
+//    }
+//
+//    public void setCreator(Long creator) {
+//        this.creator = creator;
+//    }
+//}
