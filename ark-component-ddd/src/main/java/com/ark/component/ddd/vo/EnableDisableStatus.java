6526f48764ff601bece34967a1c0f2dabdf7caef
javisChen <javischen9548@foxmail.com>
2024-01-23 00:56:55 +0800

    update components

---
+++ ark-component-ddd/src/main/java/com/ark/component/ddd/vo/EnableDisableStatus.java
@@ -0,0 +1,31 @@
+package com.ark.component.ddd.vo;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * 启用和禁用状态值对象
+ */
+@Getter
+@RequiredArgsConstructor
+public enum EnableDisableStatus {
+
+    DISABLED(0),
+    ENABLED(1);
+
+    private Integer value;
+
+    EnableDisableStatus(int value) {
+        this.value = value;
+    }
+
+    public EnableDisableStatus from(Integer value) {
+        if (DISABLED.value.equals(value)) {
+            return DISABLED;
+        }
+        if (ENABLED.value.equals(value)) {
+            return ENABLED;
+        }
+        return null;
+    }
+}
