c6b8461b08080ead0a73adfd09ca9744ac646473
javisChen <javischen9548@foxmail.com>
2024-07-02 01:04:30 +0800

    update components

---
+++ ark-component-tree/ark-component-tree-spring-boot-starter/src/main/java/com/ark/component/tree/autoconfigure/TreeAutoConfiguration.java
@@ -0,0 +1,12 @@
+package com.ark.component.tree.autoconfigure;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class TreeAutoConfiguration {
+
+    public TreeAutoConfiguration() {
+        log.info("enable [ark-component-tree-spring-boot-starter]");
+    }
+
+}
