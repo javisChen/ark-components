60050fdabf3ea5149c1f4aa88d468429bbcd0ac6
chengmo.cjw <javischen9548@foxmail.com>
2024-12-31 14:11:23 +0800

    update

--- ark-component-security/ark-component-security-base/src/main/java/com/ark/component/security/base/user/AuthUser.java
+++ ark-component-security/ark-component-security-base/src/main/java/com/ark/component/security/base/user/AuthUser.java
@@ -22,10 +22,10 @@ public class AuthUser implements UserDetails, CredentialsContainer {
     private String password;
     private String username;
     private Set<GrantedAuthority> authorities;
-    private boolean accountNonExpired;
-    private boolean accountNonLocked;
-    private boolean credentialsNonExpired;
-    private boolean enabled;
+    private boolean accountNonExpired = true;
+    private boolean accountNonLocked = true;
+    private boolean credentialsNonExpired = true;
+    private boolean enabled = true;
 
     @Override
     public void eraseCredentials() {
