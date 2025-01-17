54fdbdc8c330fd3b7f3a1d4509b34759ea589cf3
chengmo.cjw <javischen9548@foxmail.com>
2025-01-17 16:01:25 +0800

    update

--- ark-component-security/ark-component-security-spring-boot-starter/src/main/java/com/ark/component/security/autoconfigure/SecurityConfiguration.java
+++ ark-component-security/ark-component-security-spring-boot-starter/src/main/java/com/ark/component/security/autoconfigure/SecurityConfiguration.java
@@ -7,14 +7,12 @@ import com.ark.component.security.core.context.repository.RedisSecurityContextRe
 import com.ark.component.security.core.token.generate.JwtTokenGenerator;
 import com.ark.component.security.core.token.generate.TokenGenerator;
 import com.ark.component.security.core.token.issuer.TokenIssuer;
-import com.ark.component.security.core.userdetails.LoginUserDetailsService;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.context.SecurityContextRepository;
 
@@ -28,12 +26,6 @@ public class SecurityConfiguration {
     @Bean
     public TokenIssuer tokenIssuer(TokenGenerator tokenGenerator) {
         return new TokenIssuer(tokenGenerator);
-    }
-
-    @Bean
-//    @ConditionalOnMissingBean
-    public LoginUserDetailsService loginUserDetailsService(NimbusJwtDecoder jwtDecoder) {
-        return new LoginUserDetailsService(jwtDecoder);
     }
 
     @Bean
