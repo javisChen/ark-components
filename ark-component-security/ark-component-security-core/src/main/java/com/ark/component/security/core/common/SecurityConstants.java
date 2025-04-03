71cd5119c127fab2a123b955b707311cda83870e
chengmo.cjw <javischen9548@foxmail.com>
2025-04-03 15:42:46 +0800

    update

--- ark-component-security/ark-component-security-core/src/main/java/com/ark/component/security/core/common/SecurityConstants.java
+++ ark-component-security/ark-component-security-core/src/main/java/com/ark/component/security/core/common/SecurityConstants.java
@@ -5,25 +5,25 @@ package com.ark.component.security.core.common;
  */
 public interface SecurityConstants {
 
-    /**
-     * 用户密码盐值
-     */
-    String PASSWORD_SALT = "6r4ogrp7th9rbyob";
-
-    /**
-     * TOKEN过期时间（秒）
-     */
-    Long TOKEN_EXPIRES_SECONDS = (long) (60 * 60 * 24 * 7);
-
-    /**
-     * JWT签名Secret
-     */
-    String JWT_SIGN_SECRET = "QnZyMXl4WGE5MFpLT2pSa1k1Y2FFZ1FDU1dMNUN1b2YySTAzMkx0ajJsVQ";
-
-    /**
-     * JWT KeyID
-     */
-    String JWT_KEY_ID = "8d8bf81d-5032-4aeb-8962-ef473fd21ab1";
+//    /**
+//     * 用户密码盐值
+//     */
+//    String PASSWORD_SALT = "6r4ogrp7th9rbyob";
+//
+//    /**
+//     * TOKEN过期时间（秒）
+//     */
+//    Long TOKEN_EXPIRES_SECONDS = (long) (60 * 60 * 24 * 7);
+//
+//    /**
+//     * JWT签名Secret
+//     */
+//    String JWT_SIGN_SECRET = "QnZyMXl4WGE5MFpLT2pSa1k1Y2FFZ1FDU1dMNUN1b2YySTAzMkx0ajJsVQ";
+//
+//    /**
+//     * JWT KeyID
+//     */
+//    String JWT_KEY_ID = "8d8bf81d-5032-4aeb-8962-ef473fd21ab1";
 
     /**
      * 请求来源Header
@@ -40,16 +40,16 @@ public interface SecurityConstants {
      */
     String ACCESS_SOURCE_SERVICE = "ark-service";
 
-    /**
-     * 值：登录用户信息
-     * 作用：用户根据token获取登录用户信息
-     * 完整：iam:lg:tk:{token}
-     */
-    String CACHE_KEY_PREFIX_LOGIN_USER_ACCESS_TOKEN = "login:user:info:";
-    /**
-     * 值：用户登录时分配的AccessToken
-     * 作用：踢出用户。根据userId查到token -> 根据token移除登录缓存
-     * 完整：iam:lg:uid:{userId}
-     */
-    String CACHE_KEY_PREFIX_LOGIN_USER_ID = "login:user:token:";
+//    /**
+//     * 值：登录用户信息
+//     * 作用：用户根据token获取登录用户信息
+//     * 完整：iam:lg:tk:{token}
+//     */
+//    String CACHE_KEY_PREFIX_LOGIN_USER_ACCESS_TOKEN = "login:user:info:";
+//    /**
+//     * 值：用户登录时分配的AccessToken
+//     * 作用：踢出用户。根据userId查到token -> 根据token移除登录缓存
+//     * 完整：iam:lg:uid:{userId}
+//     */
+//    String CACHE_KEY_PREFIX_LOGIN_USER_ID = "login:user:token:";
 }
